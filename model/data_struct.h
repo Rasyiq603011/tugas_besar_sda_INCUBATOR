01197e41380b49db222726dae4b29421508dfdff
Rasyiq03 <mns.rasyiq03@gmail.com>
2025-05-29 16:40:56 +0700

    بسم الله

---
+++ model/data_struct.h
@@ -0,0 +1,30 @@
+#ifndef DATA_STRUCT_H
+#define DATA_STRUCT_H
+
+
+typedef struct data_provinsi
+{
+    /* data */
+}Provinsi;
+
+typedef struct data_kota
+{
+    /* data */
+}Kota;
+
+typedef struct data_bioskop
+{
+    /* data */
+}Bioskop;
+
+typedef struct data_film
+{
+    /* data */
+}Film;
+
+typedef struct data_negara
+{
+    /* data */
+}Negara;
+
+#endif
---
+++ model/tree_data.h
@@ -0,0 +1,17 @@
+#ifndef TREE_DATA_H
+#define TREE_DATA_H
+#include "data_struct.h"
+
+typedef union
+{
+    /* data */
+}infotype;
+
+
+typedef struct treeElmmt
+{
+    /* data */
+}Tree;
+
+
+#endif
