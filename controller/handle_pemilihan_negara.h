3f013c5add7813157d2171e1ae12154ff8deea2a
Arassy <muhammadarrafi@Muhammads-MacBook-Air.local>
2025-06-12 15:17:11 +0700

    bismillah tampilan

---
+++ controller/handle_pemilihan_negara.h
@@ -0,0 +1,11 @@
+#ifndef HANDLE_PEMILIHAN_NEGARA
+#define HANDLE_PEMILIHAN_NEGARA
+
+#include "../view/view.h"
+#include "../model/data_struct/negara.h"
+
+
+
+
+
+#endif /*HANDLE_PEMILIHAN_NEGARA*/
---
+++ view/tampilan_negara.h
@@ -0,0 +1,9 @@
+#ifndef TAMPILAN_NEGARA_H
+#define TAMPILAN_NEGARA_H
+
+#include "../model/tree_data.h"
+
+
+void tampilan_negara(int idx, int y, int selected, void* pointer_head);
+
+#endif // TAMPILAN_NEGARA_H
