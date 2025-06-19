c35c3b65cd732f398f3941c74ac62b27697e355a
Ivan Setiawan Ihsan <ivansetiawan2975@gmail.com>
2025-06-19 11:54:26 +0700

    Menambah implementasi handle riwayat

--- src/controller/handle_login_register.h
+++ src/controller/handle_login_register.h
@@ -1,7 +1,6 @@
 #ifndef HANDLE_LOGIN_REGISTER_H
 #define HANDLE_LOGIN_REGISTER_H
 
-#include "handle_login_register.h"
 #include "../view/tampilan_login_signup.h"
 #include "../model/tree_data.h"
 #include "../model/file_manipulation/write_json.h"
--- src/controller/handle_tampilan_riwayat.c
+++ src/controller/handle_tampilan_riwayat.c
@@ -1,6 +1,10 @@
 #include "handle_tampilan_riwayat.h"
 
-void handle_tampilan_riwayat(Riwayat* head)
+void handle_tampilan_riwayat(User* user)
 {
-//    scrollable_menu(get_jumlah_riwayat(head), tampilan_riwayat, 7, 4, head);
+    void** array;
+    int jumlah = convert_riwayat_to_array(get_pointer_to_riwayat(user), &array);
+    int pilihan = scrollable_menu(array, jumlah, tampilan_riwayat, "Riwayat", 8, 4);
+    free(array);
+    return;
 }
