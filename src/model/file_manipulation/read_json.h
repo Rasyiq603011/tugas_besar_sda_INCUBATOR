6ca591f726f518a4338ce55688b86de455691f61
Ivan Setiawan Ihsan <ivansetiawan2975@gmail.com>
2025-06-19 10:28:10 +0700

    Update file manipulation

--- src/model/file_manipulation/read_json.h
+++ src/model/file_manipulation/read_json.h
@@ -9,9 +9,13 @@
 #include "../tree_data.h"
 
 #define FILENAME "../../assets/bioskop_database.json"
+#define DATABASE_USER "../assets/users.json"
 
 // === INTERFACE ===
 address load_tree_from_file(const char* filename);
 address json_to_tree(cJSON* json, DataType tipe);
+User* load_user_from_json(const char* username_target);
+bool is_username_available(const char* username_to_check);
+bool is_password_correct(const char* username, const char* password_input);
 
 #endif /* READ_JSON_H */
